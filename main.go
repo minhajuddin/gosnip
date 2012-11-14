a47873af74ed9614f52eaf9da9dfc04a550b646a
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2012-11-14 15:53:27 +0530

    breaking commit

--- main.go
+++ main.go
@@ -10,22 +10,42 @@ import (
 	"html/template"
 
 	"code.google.com/p/gorilla/mux"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 )
 
+//Stores a single instance of a code snippet
 type Snippet struct{
 	Name string
 	Description string
 	Code string
 }
 
+//Returns a list of all snippets stored in the database
+//TODO: add pagination
+func AllSnippets() []Snippet {
+	return data
+}
+
+var snippetsCollection mgo.Collection
+
 var data []Snippet
 var indexTemplate, err = template.ParseFiles("views/index.html")
 
 func homeHandler(rw http.ResponseWriter, req * http.Request){
-	indexTemplate.Execute(rw, data)
+	indexTemplate.Execute(rw, AllSnippets())
 }
 
 func main(){
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer session.Close()
+
+	snippetsCollection := session.DB("gosnips").C("snippets")
+
+
 	data = make([]Snippet, 100)
 	data[0] = Snippet{"helloworld", "Hello world snip", `
 		package main
