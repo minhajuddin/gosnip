8020e957153295df35b1e4150f68e952b0881943
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2012-11-14 13:14:57 +0530

    Added an html template

--- main.go
+++ main.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"strconv"
-	"fmt"
+	//"fmt"
 	"code.google.com/p/gorilla/mux"
 	"net/http"
 	"flag"
+	"log"
+	"html/template"
+	"time"
 )
 
+var indexTemplate, err = template.ParseFiles("views/index.html")
+
 func homeHandler(rw http.ResponseWriter, req * http.Request){
-	rw.Write([]byte("This is an awesome response"))
+	indexTemplate.Execute(rw, struct{Time time.Time}{time.Now()})
 }
 
 func main(){
 	port := flag.Int("port", 3050, "port to run snippet server")
 	flag.Parse()
 
-	fmt.Println("started server on", *port)
+	log.Println("started server on", *port)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
